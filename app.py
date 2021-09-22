4966232c6a9a8640c956d5be125d43af218c3d03
DSHIHK <darryl@dshi.tech>
2021-09-22 17:53:42 +0800

    Added name

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 st.title("Darryl's Web App")
 st.write("Not gonna lie, haven't done coding in a hell long time. I'm probably going to screw this up... big time")
+with st.form(key='my_form'):
+    text_input = st.text_input(label='Enter your name')
+    submit_button = st.form_submit_button(label='Submit')
