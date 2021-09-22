c5d64b7604c52669801fb3d4509fc92e10b535f9
DSHIHK <darryl@dshi.tech>
2021-09-22 17:56:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,3 @@ import pandas as pd
 import time
 st.title("Darryl's Web App")
 st.write("Not gonna lie, haven't done coding in a hell long time. I'm probably going to screw this up... big time")
-with st.form(key='my_form'):
-    name = st.text_input(label='Enter your name')
-    submit_button = st.form_submit_button(label='Submit')
-    st.write('Hello, ' + name)
-    time.sleep(1)
-    email = st.text_input(label='What is your email?')
-    submit_button = st.form_submit_button(label='Submit')
